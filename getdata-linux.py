6f36c52a4233c44180c95c6c08a1b736a5087d08
Konano <nanoapezlk@gmail.com>
2018-05-01 18:06:05 +0800

    upload

--- getdata-linux.py
+++ getdata-linux.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding:utf-8 -*-
 
-import time
+import time,platform
 import codecs
 import os
 from selenium import webdriver
@@ -48,7 +48,7 @@ def get_data():
 
 	output(Data)
 
-if __name__ == '__main__':
+def funzione():
 
 	display = Display(visible=0, size=(800,600))
 	display.start()
@@ -61,3 +61,40 @@ if __name__ == '__main__':
 
 	driver.quit()
 	display.stop()
+
+def createDaemon():
+	# fork进程
+	try:
+		if os.fork() > 0: os._exit(0)
+	except OSError, error:
+		print 'fork #1 failed: %d (%s)' % (error.errno, error.strerror)
+		os._exit(1)
+	os.chdir('/')
+	os.setsid()
+	os.umask(0)
+	try:
+		pid = os.fork()
+		if pid > 0:
+			print 'Daemon PID %d' % pid
+			os._exit(0)
+	except OSError, error:
+		print 'fork #2 failed: %d (%s)' % (error.errno, error.strerror)
+		os._exit(1)
+	# 重定向标准IO
+	sys.stdout.flush()
+	sys.stderr.flush()
+	si = file("/dev/null", 'r')
+	so = file("/dev/null", 'a+')
+	se = file("/dev/null", 'a+', 0)
+	os.dup2(si.fileno(), sys.stdin.fileno())
+	os.dup2(so.fileno(), sys.stdout.fileno())
+	os.dup2(se.fileno(), sys.stderr.fileno())
+
+	# 在子进程中执行代码
+	funzione() # function demo
+
+if __name__ == '__main__':
+	if platform.system() == "Linux":
+		createDaemon()
+	else:
+		os._exit(0)
